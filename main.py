9baf3f2abfbd8a96c0c6b02f43bf731ebd57af0c
Duc Trinh <duc.trinh.1501@gmail.com>
2021-09-27 15:52:06 -0400

    Starting the template for the manuscript viewing page

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ import json
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
+
+@app.route("/")
+def index(request: Request):
+    context = dict(
+        request = request,
+        my_string = "Wheeeee!",
+        my_list = [0, 1, 2, 3, 4, 5],
+    )
+    return templates.TemplateResponse("manu_list.html", context)
