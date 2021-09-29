1b2380c9fa822e096296a8f246a3010d6f744269
Duc Trinh <duc.trinh.1501@gmail.com>
2021-09-29 17:17:09 -0400

    Linked the manuscripts with the table on the index page

--- main.py
+++ main.py
@@ -1,17 +1,43 @@
 from fastapi import FastAPI, Request, HTTPException
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel, Json
-import json
+from typing import Optional
+import srsly
+from pathlib import Path
 
 app = FastAPI()
-
 templates = Jinja2Templates(directory="templates")
+
+
+class Manuscript(BaseModel):
+    arab_title: Optional[str]
+    chinese_title: Optional[str]
+    people_involved: Optional[str]
+    type: Optional[str]
+    place: Optional[str]
+    year: Optional[str]
+    publisher: Optional[str]
+    num_pages: Optional[str]
+
+
+def get_data():
+    data_dir = Path.cwd() / 'data'
+    manuscripts = []
+    for item in data_dir.iterdir():
+        data = srsly.read_json(item)
+        if data:
+            item = Manuscript(**data)
+            manuscripts.append(item)
+        else:
+            raise FileNotFoundError("Manuscript data file is missing")
+    return manuscripts
+
 
 @app.route("/")
 def index(request: Request):
+    manuscripts = get_data()
     context = dict(
-        request = request,
-        my_string = "Wheeeee!",
-        my_list = [0, 1, 2, 3, 4, 5],
+        request=request,
+        manuscripts=manuscripts,
     )
     return templates.TemplateResponse("manu_list.html", context)
