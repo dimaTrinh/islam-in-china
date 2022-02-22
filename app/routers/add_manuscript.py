3da2164ff33220dde4d41330587de27f494ebb7d
Duc Trinh <duc.trinh.1501@gmail.com>
2022-02-22 12:04:07 -0500

    Changed the upload to accept a list of image files instead of a pdf

--- app/routers/add_manuscript.py
+++ app/routers/add_manuscript.py
@@ -5,6 +5,7 @@ from app.util.handle_data_from_csv import get_data_from_csv, write_data_to_csv
 from app.util.models import get_data
 from app.util.convert_pdf_to_images import pdf_to_images
 from app.util.generate_manifest_from_json import generate_ind_manifest
+from app.util.save_images_to_disk import save_images
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
@@ -32,28 +33,30 @@ async def handle_form(request: Request,
                       place: str = Form(None),
                       publisher: str = Form(None),
                       year: str = Form(None),
-                      stand_year: int = Form(None),
+                      stand_year: int = Form(0),
                       language: str = Form(None),
                       description: str = Form(None),
                       notes: str = Form(None),
-                      manu_file: UploadFile = File(...)):
-    manu_content = await manu_file.read()
-
+                      image_files: list[UploadFile] = Form(...),
+                      ):
     # get the index for the new text
     new_manu_ind = str(await get_data_from_csv(write_file=False) + 1)
+
+    # get the internal id we use for manuscript file
     new_manu_id = "text_{}".format(new_manu_ind.zfill(3))
 
-    # save the pdf to images on the server
-    num_pdf_pages = await pdf_to_images(manu_content, new_manu_id)
+    # process and save the images to disk
+    num_pages = await save_images(new_manu_id, image_files)
+
     # generate new row to be written to the csv file from the form
     new_row = [new_manu_id, arab_title, chinese_title, author, assembler, editor,
                scrivener, translator, type, place, publisher, year, stand_year, language,
-               num_pdf_pages, description, notes]
+               num_pages, description, notes]
     await write_data_to_csv(new_row)
 
-    # generate new data for the site and new manifest
+    # generate the new metadata for the site along with its manifest
     await get_data_from_csv(write_file=True)
-    await generate_ind_manifest(new_manu_id, num_pdf_pages)
+    await generate_ind_manifest(new_manu_id, num_pages)
 
     manuscripts, idx_dict = await get_data()
     context = dict(
---
+++ app/util/save_images_to_disk.py
@@ -0,0 +1,23 @@
+import os
+import aiofiles
+from pathlib import Path
+
+
+async def save_images(text_id, image_files):
+    # get the image names and the content of the image files from the uploaded file
+    # remove the extension of the image
+    images = [(os.path.splitext(file.filename)[0], await file.read()) for file in image_files]
+
+    # sort the images alphabetically by file name
+    images.sort()
+
+    # save each page to the asset folder
+    for (index, (name, image_content)) in enumerate(images):
+        image_dir = Path.cwd() / 'assets' / 'img' / 'texts' / '{}_page_{}.jpg'.format(new_manu_id, pg_number)
+        async with aiofiles.open(image_dir, 'wb') as out_file:
+            pg_number = str(index + 1).zfill(3)
+            await out_file.write(image_content)
+
+    num_pages = len(images)
+
+    return num_pages
