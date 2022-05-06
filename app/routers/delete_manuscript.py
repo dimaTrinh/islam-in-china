2010efd5465596bc3512b9eba5169fc3c0bbc6f2
Duc Trinh <duc.trinh.1501@gmail.com>
2022-05-06 17:42:02 -0400

    Testing the manuscript delete function

--- app/routers/delete_manuscript.py
+++ app/routers/delete_manuscript.py
@@ -1,5 +1,6 @@
 from fastapi import Request, APIRouter, Depends, Form
 from fastapi.templating import Jinja2Templates
+from pathlib import Path
 
 from app.util.login import get_current_username
 from app.util.models import get_data
@@ -22,6 +23,20 @@ async def get_delete_manu(request: Request, username: str = Depends(get_current_
 
 @router.get("/delete_manuscript/{text_id}")
 async def delete_manu(request: Request, text_id: str):
+    # delete the manuscript from the spreadsheet
+    delete_data_from_spreadsheet(text_id)
+
+    # delete all images that start with the text_id
+    image_dir = Path.cwd() / 'assets' / 'img' / 'texts'
+    for file in image_dir.glob('{}*'.format(text_id)):
+        file.unlink()
+
+    # delete the json files that have the the text_id
+    data_dir = Path.cwd() / 'data'
+    for file in data_dir.rglob('{}.json'.format(text_id)):
+        file.unlink()
+
+    # refresh the data then return back to the delete manuscript site
     manuscripts, idx_dict = get_data()
     context = dict(
         request=request,
